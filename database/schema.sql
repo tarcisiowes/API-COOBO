4ddc0eba8bf8491eba63ff9baa84cd03925f3ef1
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-31 13:26:51 -0300

    criado banco de dados + coluna deleted

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,11 @@
+CREATE DATABASE coobo;
+
 create table if not exists documents (
   id serial primary key,
   kbSize integer,
   nome unique text not null,
   content text,
   createdAt text not null,
-  deleteAt text not null
+  deleteAt text not null,
+  deleted boolean default false
 )
