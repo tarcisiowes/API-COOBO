e7fd16a5a719e5137893fbf502282d03bea54dbc
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-09-01 12:36:54 -0300

    adicionado try/catch + tratamento de erros + conversao base64+ kbsize

--- src/controladores/adicionar.js
+++ src/controladores/adicionar.js
@@ -1,22 +1,33 @@
 const knex = require('../../database/conexao');
 
 const adicionarDocumento = async (req, res) => {
-  const {
-    name, content, kbSize,
+  let {
+    name, content,
   } = req.body;
 
   const timeElapsed = Date.now();
   const today = new Date(timeElapsed);
-  const createdAt = today.toDateString();
+  const createdat = today.toDateString();
 
-  const resposta = await knex('documents').insert({
-    name, content, createdAt, kbSize,
-  });
+  content = new Buffer.from(content).toString('base64');
 
-  if (resposta.rowCount === 0) {
-    return res.status(404).json('Não foi possivel adicionar o documento, tente novamente');
+  function byteCount(value) {
+    return encodeURI(value).length - 1;
   }
-  return res.json(resposta);
+  const kbsize = Math.round(byteCount(content) / 1.37) / 1000;
+
+  try {
+    const resposta = await knex('documents').insert({
+      name, content, createdat, kbsize,
+    });
+
+    if (resposta.rowCount === 0) {
+      return res.status(404).json('Não foi possivel adicionar o documento, tente novamente');
+    }
+    return res.json('Documento criado com sucesso!');
+  } catch (error) {
+    return res.status(404).json(`Erro ${error}`);
+  }
 };
 
 module.exports = { adicionarDocumento };
