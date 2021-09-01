4e432e4efe55918f9d8b2a2385f4fdd3282d42ca
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-09-01 12:30:42 -0300

    adequando regras do banco

--- database/schema.sql
+++ database/schema.sql
@@ -2,10 +2,10 @@ CREATE DATABASE coobo;
 
 create table if not exists documents (
   id serial primary key,
-  kbSize integer,
-  nome unique text not null,
+  kbSize numeric,
+  name text not null unique,
   content text,
   createdAt text not null,
-  deleteAt text not null,
+  deleteAt text ,
   deleted boolean default false
 )
