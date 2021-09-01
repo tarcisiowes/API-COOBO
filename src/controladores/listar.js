3584808ada822738e9426b4f2643c74795e756ce
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-09-01 12:35:39 -0300

    adicionado try/catch + tratamento de erros

--- src/controladores/listar.js
+++ src/controladores/listar.js
@@ -1,11 +1,15 @@
 const knex = require('../../database/conexao');
 
 const listarDocumentos = async (req, res) => {
-  const resposta = await knex('documents').where('deleted', false);
-  if (resposta.rowCount === 0) {
-    return res.status(404).json('Não foi encontrado nenhum documento');
+  try {
+    const resposta = await knex('documents').where('deleted', false);
+    if (resposta.rowCount === 0) {
+      return res.status(404).json('Não foi encontrado nenhum documento');
+    }
+    return res.json(resposta);
+  } catch (error) {
+    return res.status(404).json(`Erro ${error}`);
   }
-  return res.json(resposta);
 };
 
 module.exports = { listarDocumentos };
