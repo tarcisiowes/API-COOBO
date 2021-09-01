6a7a7d7ee401aa6e2b9423ec1e783e0493b31590
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-09-01 12:35:28 -0300

    adicionado try/catch + tratamento de erros

--- src/controladores/buscar.js
+++ src/controladores/buscar.js
@@ -2,12 +2,16 @@ const knex = require('../../database/conexao');
 
 const buscarDocumento = async (req, res) => {
   const { id } = req.params;
-  const resposta = await knex('documents').where({ id }).where('deleted', false);
+  try {
+    const resposta = await knex('documents').where({ id }).where('deleted', false);
 
-  if (resposta.rowCount === 0) {
-    return res.status(404).json('O documento do ID infomado não existe');
+    if (resposta.rowCount === 0) {
+      return res.status(404).json('O documento do ID infomado não existe');
+    }
+    return res.json(resposta);
+  } catch (error) {
+    return res.status(404).json(`Erro ${error}`);
   }
-  return res.json(resposta);
 };
 
 module.exports = { buscarDocumento };
