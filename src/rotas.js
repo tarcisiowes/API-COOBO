0b06e324b9e5e2b25f8c2be504a186776caf527b
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-09-01 12:32:05 -0300

    adicionado rota dia da semana

--- src/rotas.js
+++ src/rotas.js
@@ -4,6 +4,7 @@ const { listarDocumentos } = require('./controladores/listar');
 const { buscarDocumento } = require('./controladores/buscar');
 const { adicionarDocumento } = require('./controladores/adicionar');
 const { deletarDocumento } = require('./controladores/deletar');
+const { diaDaSemana } = require('./controladores/diaDaSemana');
 
 const rotas = express();
 
@@ -11,5 +12,6 @@ rotas.get('/documents', listarDocumentos);
 rotas.get('/documents/:id', buscarDocumento);
 rotas.post('/documents', adicionarDocumento);
 rotas.patch('/documents/:id', deletarDocumento);
+rotas.get('/weekday-after', diaDaSemana);
 
 module.exports = rotas;
